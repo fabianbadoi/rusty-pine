ad836c9b774756e9651d76e9a5fe2f4a5c95ad86
Fabian Badoi <fb@penneo.com>
2019-04-27 14:39:17 +0200

    Clean up pest_tree_translation

--- src/pine_syntax/pest_tree_translation.rs
+++ src/pine_syntax/pest_tree_translation.rs
@@ -2,61 +2,61 @@ use super::Pine;
 use super::{Positioned, Position};
 use super::{Operation, TableName, Filter, FilterNode, Condition, ConditionNode, Value};
 use super::pest::Rule;
-use ::pest::iterators::Pair as PestPair;
+use ::pest::iterators::Pair;
 
-type Pair<'a> = PestPair<'a, Rule>;
+type Node<'a> = Pair<'a, Rule>;
 
-pub fn translate(root_pair: Pair) -> Pine {
-    expect(Rule::pine, &root_pair);
+pub fn translate(root_node: Node) -> Pine {
+    expect(Rule::pine, &root_node);
 
-    let position = pair_to_position(&root_pair);
-    let operations : Vec<_> = root_pair.into_inner().map(translate_operation).collect();
+    let position = node_to_position(&root_node);
+    let operations : Vec<_> = root_node.into_inner().map(translate_operation).collect();
 
     Pine { position, inner: operations }
 }
 
-fn translate_operation(pair: Pair) -> Positioned<Operation> {
-    expect(Rule::operation, &pair);
+fn translate_operation(node: Node) -> Positioned<Operation> {
+    expect(Rule::operation, &node);
     
-    let operation_pair = pair.into_inner().next()
+    let operation_node = node.into_inner().next()
         .expect("Pest should not have created an operation without an inner");
 
-    let position = pair_to_position(&operation_pair);
-    let operation = match operation_pair.as_rule() {
-        Rule::from => translate_from(operation_pair),
-        Rule::select => translate_select(operation_pair),
-        Rule::filters => translate_filters(operation_pair),
-        _ => panic!("Expected a operation variant, got '{:?}'", operation_pair.as_rule())
+    let position = node_to_position(&operation_node);
+    let operation = match operation_node.as_rule() {
+        Rule::from => translate_from(operation_node),
+        Rule::select => translate_select(operation_node),
+        Rule::filters => translate_filters(operation_node),
+        _ => panic!("Expected a operation variant, got '{:?}'", operation_node.as_rule())
     };
 
     Positioned { position, inner: operation }
 }
 
-fn translate_from(pair: Pair) -> Operation {
+fn translate_from(node: Node) -> Operation {
     let table_name = translate_sql_name(
-        pair.into_inner().next().expect("Found from without table name")
+        node.into_inner().next().expect("Found from without table name")
     );
 
     Operation::From(table_name)
 }
 
-fn translate_select(pair: Pair) -> Operation {
-    let columns : Vec<_> = pair.into_inner().map(translate_sql_name).collect();
+fn translate_select(node: Node) -> Operation {
+    let columns : Vec<_> = node.into_inner().map(translate_sql_name).collect();
 
     Operation::Select(columns)
 }
 
-fn translate_filters(pair: Pair) -> Operation {
-    let filters : Vec<_> = pair.into_inner().map(translate_filter).collect();
+fn translate_filters(node: Node) -> Operation {
+    let filters : Vec<_> = node.into_inner().map(translate_filter).collect();
 
     Operation::Filter(filters)
 }
 
-fn translate_filter(pair: Pair) -> FilterNode {
-    expect(Rule::filter, &pair);
+fn translate_filter(node: Node) -> FilterNode {
+    expect(Rule::filter, &node);
 
-    let position = pair_to_position(&pair);
-    let mut parts : Vec<_> = pair.into_inner().collect();
+    let position = node_to_position(&node);
+    let mut parts : Vec<_> = node.into_inner().collect();
 
     if parts.len() != 2 {
         panic!("Filters must have 2 parts: a column and a condition. Found:\n{:#?}", parts);
@@ -73,47 +73,47 @@ fn translate_filter(pair: Pair) -> FilterNode {
     FilterNode { inner, position }
 }
 
-fn translate_condition(pair: Pair) -> ConditionNode {
-    expect(Rule::equals, &pair);
+fn translate_condition(node: Node) -> ConditionNode {
+    expect(Rule::equals, &node);
 
-    let position = pair_to_position(&pair);
-    let value = translate_value(pair.into_inner().next().expect("For now, conditions must have a value"));
+    let position = node_to_position(&node);
+    let value = translate_value(node.into_inner().next().expect("For now, conditions must have a value"));
     let inner = Condition::Equals(value);
 
     ConditionNode { position, inner }
 }
 
-fn translate_value(pair: Pair) -> Value {
-    expect(Rule::numeric_value, &pair);
+fn translate_value(node: Node) -> Value {
+    expect(Rule::numeric_value, &node);
 
-    let position = pair_to_position(&pair);
-    let inner = pair.as_str().trim().to_string();
+    let position = node_to_position(&node);
+    let inner = node.as_str().trim().to_string();
 
     Value { inner, position }
 }
 
-fn translate_sql_name(pair: Pair) -> TableName {
-    expect_one_of(vec![Rule::column_name, Rule::table_name], &pair);
+fn translate_sql_name(node: Node) -> TableName {
+    expect_one_of(vec![Rule::column_name, Rule::table_name], &node);
 
-    let position = pair_to_position(&pair);
+    let position = node_to_position(&node);
 
-    TableName { inner: pair.as_str().to_string(), position }
+    TableName { inner: node.as_str().to_string(), position }
 }
 
-fn expect(expected_type: Rule, pair: &Pair) {
-    if pair.as_rule() != expected_type {
-        panic!("Token be a '{:?}' expression, found '{:?}'", expected_type, pair.as_rule());
+fn expect(expected_type: Rule, node: &Node) {
+    if node.as_rule() != expected_type {
+        panic!("node be a '{:?}' expression, found '{:?}'", expected_type, node.as_rule());
     }
 }
 
-fn expect_one_of(expected_types: Vec<Rule>, pair: &Pair) {
-    if !expected_types.contains(&pair.as_rule()) {
-        panic!("Token be a one of {:?}, found '{:?}'", expected_types, pair.as_rule());
+fn expect_one_of(expected_types: Vec<Rule>, node: &Node) {
+    if !expected_types.contains(&node.as_rule()) {
+        panic!("node be a one of {:?}, found '{:?}'", expected_types, node.as_rule());
     }
 }
 
-fn pair_to_position(pair: &Pair) -> Position {
-    let span = pair.as_span();
+fn node_to_position(node: &Node) -> Position {
+    let span = node.as_span();
 
     Position {start: span.start(), end: span.end() }
 }
