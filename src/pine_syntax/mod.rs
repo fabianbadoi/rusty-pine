d0995133e1513cb91626236b5084764007f5939e
Fabian Badoi <fb@penneo.com>
2019-04-28 22:04:19 +0200

    Refactor pine_syntax::PineParser

--- src/pine_syntax/mod.rs
+++ src/pine_syntax/mod.rs
@@ -20,13 +20,13 @@ impl From<PestError<Rule>> for PineParseError {
 }
 
 pub trait PineParserTrait {
-    fn parse(input: &str) -> Result<PineNode, PineParseError>;
+    fn parse(self, input: &str) -> Result<PineNode, PineParseError>;
 }
 
 struct PineParser;
 
-impl PineParserTrait for PineParser {
-    fn parse(input: &str) -> Result<PineNode, PineParseError> {
+impl PineParserTrait for &PineParser {
+    fn parse(self, input: &str) -> Result<PineNode, PineParseError> {
         let ast = pest::PinePestParser::parse(pest::Rule::pine, input)?
             .next()
             .expect("Pest should have failed to parse this input");
@@ -43,8 +43,9 @@ mod tests {
 
     #[test]
     fn parsing_simple_form_statement() {
+        let parser = PineParser{};
         let pine_node =
-            PineParser::parse("from: users | select: id, name | where: id = 3 x = 4").unwrap();
+            parser.parse("from: users | select: id, name | where: id = 3 x = 4").unwrap();
 
         assert_eq!("from", pine_node.inner.operations[0].inner.get_name());
         assert_eq!("select", pine_node.inner.operations[1].inner.get_name());
