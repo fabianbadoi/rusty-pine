9e4abefb23959dd592683455997cc5537d42760e
Fabian Badoi <fabian.badoi@gmail.com>
2019-06-08 09:14:54 +0200

    Add stand in main function

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,48 @@ mod query;
 mod sql;
 mod error;
 
-
 fn main() {
-    use pine_parser::{Parser, GenericParser};
+    use pine_parser::{NaiveParser, Parser};
 
-    let parser = GenericParser::default();
+    let parser = NaiveParser::default();
 
-    println!("{}", parser.parse("from: users | where: id = 3 | select: id").unwrap());
-    println!("{}", parser.parse("from: users | filter: id = 3 | select: id").unwrap_err().message);
+    // normal flow
+    println!("------------------------------");
+    println!("{}", parser.parse("from: users | where: id = 3").unwrap());
+    println!("------------------------------");
+
+    // syntax error 1
+    println!("------------------------------");
+    println!(
+        "{}",
+        parser
+            .parse("from: users | filter: id = 3 | select: id")
+            .unwrap_err()
+    );
+    println!("------------------------------");
+
+    // syntax erro 2
+    println!("------------------------------");
+    println!(
+        "{}",
+        parser
+            .parse("from: users | where: id  3 3 id | select: id")
+            .unwrap_err()
+    );
+    println!("------------------------------");
+
+    // query builder flow
+    println!("------------------------------");
+    println!(
+        "{}",
+        parser.parse("where: id = 3 | select: id").unwrap_err()
+    );
+    println!("------------------------------");
+
+    println!("------------------------------");
+    println!(
+        "{}",
+        parser.parse("users 3").unwrap()
+    );
+    println!("------------------------------");
 }
