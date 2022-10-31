7720b9a240ffe0902db21de963bbef5cfee24103
Fabian Badoi <fb@penneo.com>
2022-10-31 15:12:02 +0100

    Add support for proper exit codes

--- apps/src/bin/main.rs
+++ apps/src/bin/main.rs
@@ -3,25 +3,38 @@ extern crate rusty_pine_lib;
 
 use rusty_pine_lib::read_config;
 use rusty_pine_lib::{offline, Transpiler};
+use std::process::ExitCode;
 
-fn main() {
+fn main() -> ExitCode {
     pretty_env_logger::init();
 
     let database = match get_database() {
         Some(string) => string,
-        None => return,
+        None => {
+            eprintln!("/*\nFirst argument must be a database name\n*/");
+            return ExitCode::FAILURE;
+        }
     };
 
     let transpiler = offline(&read_config(), database.as_str()).unwrap();
 
     let input = match get_input_pine() {
         Some(string) => string,
-        None => return,
+        None => {
+            eprintln!("/*\nSecond argument must be a database Pine\n*/");
+            return ExitCode::FAILURE;
+        }
     };
 
     match transpiler.transpile(input.as_ref() as &str) {
-        Ok(query) => println!("{}", query),
-        Err(error) => println!("/*\n{}\n*/", error),
+        Ok(query) => {
+            println!("{}", query);
+            ExitCode::SUCCESS
+        }
+        Err(error) => {
+            eprintln!("/*\n{}\n*/", error);
+            ExitCode::FAILURE
+        }
     }
 }
 
