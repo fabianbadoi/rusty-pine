9fc6eea0a8de3b3b020153b74c15641bb810e398
Fabian Badoi <fb@penneo.com>
2023-05-19 12:53:18 +0200

    Move db, table, and column translation stuff into its own mod for stage2

--- gen2/rusty-pine/src/syntax/stage2.rs
+++ gen2/rusty-pine/src/syntax/stage2.rs
@@ -22,6 +22,8 @@ use crate::syntax::{
 use pest::iterators::{Pair, Pairs};
 use pest::Span;
 
+mod identifiers;
+
 /// ```rust
 /// # use crate::syntax::stage1::parse_stage1;
 /// # let stage1_rep = parse_stage1("name").unwrap();
@@ -76,7 +78,7 @@ fn translate_base(mut base_pair: Pair<Rule>) -> Positioned<Stage2Pine> {
     assert_eq!(Rule::base, base_pair.as_rule());
 
     let position: Position = base_pair.as_span().into();
-    let table_name = translate_table(base_pair.into_inner().next().unwrap());
+    let table_name = identifiers::translate_table(base_pair.into_inner().next().unwrap());
 
     position.holding(Stage2Pine::Base { table: table_name })
 }
@@ -94,134 +96,9 @@ fn translate_select(select: Pair<Rule>) -> Positioned<Stage2Pine> {
     let position: Position = select.as_span().into();
     let column_pair = select.into_inner().next().expect("Has to be valid syntax");
 
-    position.holding(Stage2Pine::Select(translate_column(column_pair)))
-}
-
-fn translate_column(column: Pair<Rule>) -> ColumnInput {
-    assert_eq!(Rule::column, column.as_rule());
-
-    let mut inners = column.into_inner();
-    let inner = inners.next().expect("Has to be valid syntax");
-    assert!(inners.next().is_none());
-
-    match inner.as_rule() {
-        Rule::db_table_column_name => translate_db_table_column_name(inner),
-        Rule::table_column_name => translate_table_column_name(inner),
-        Rule::column_name => translate_column_name(inner),
-        _ => panic!("Unknown column type {:#?}", inner.as_rule()),
-    }
-}
-
-fn translate_column_name(pair: Pair<Rule>) -> ColumnInput {
-    assert_eq!(Rule::column_name, pair.as_rule());
-
-    let position: Position = pair.as_span().into();
-
-    let mut inners = pair.into_inner();
-
-    let table = Implicit;
-    let column = translate_sql_name(inners.next().unwrap());
-
-    assert!(inners.next().is_none());
-
-    ColumnInput {
-        table,
-        column,
-        position,
-    }
-}
-
-fn translate_table_column_name(pair: Pair<Rule>) -> ColumnInput {
-    assert_eq!(Rule::table_column_name, pair.as_rule());
-
-    let position: Position = pair.as_span().into();
-
-    let mut inners = pair.into_inner();
-
-    let table_name = translate_sql_name(inners.next().unwrap());
-    let table = Specified(TableInput {
-        table: table_name,
-        position: table_name.position,
-        database: Implicit,
-    });
-    let column = translate_sql_name(inners.next().unwrap());
-
-    ColumnInput {
-        table,
-        column,
-        position,
-    }
-}
-
-fn translate_db_table_column_name(pair: Pair<Rule>) -> ColumnInput {
-    assert_eq!(Rule::db_table_column_name, pair.as_rule());
-
-    let position: Position = pair.as_span().into();
-
-    let mut inners = pair.into_inner();
-
-    let table = Specified(translate_table(inners.next().unwrap()));
-    let column = translate_sql_name(inners.next().unwrap());
-
-    ColumnInput {
-        table,
-        column,
-        position,
-    }
-}
-
-fn translate_table(name_pair: Pair<Rule>) -> TableInput {
-    let mut inners = name_pair.into_inner();
-    let inner = inners.next().expect("No pairs should be invalid syntax");
-    assert!(
-        inners.next().is_none(),
-        "Multiple pairs should be invalid syntax"
-    );
-
-    match inner.as_rule() {
-        Rule::sql_name => translate_table_sql_name(inner),
-        Rule::db_table_name => translate_db_table_name(inner),
-        _ => panic!("Unsupported rule: {:?}", inner.as_rule()),
-    }
-}
-
-fn translate_table_sql_name(pair: Pair<Rule>) -> TableInput {
-    assert_eq!(Rule::sql_name, pair.as_rule());
-
-    let position = pair.as_span().into();
-
-    TableInput {
-        database: OptionalInput::Implicit,
-        table: translate_sql_name(pair),
-        position,
-    }
-}
-
-fn translate_sql_name(pair: Pair<Rule>) -> SqlIdentifierInput {
-    assert_eq!(Rule::sql_name, pair.as_rule());
-
-    let position = pair.as_span().into();
-
-    SqlIdentifierInput {
-        name: pair.as_str(),
-        position,
-    }
-}
-
-fn translate_db_table_name(pair: Pair<Rule>) -> TableInput {
-    assert_eq!(Rule::db_table_name, pair.as_rule());
-
-    let position = pair.as_span().into();
-
-    let mut inners = pair.into_inner();
-    let db_name_pair = inners.next().expect("No db should be invalid syntax");
-    let table_name_pair = inners.next().expect("No table should be invalid syntax");
-
-    TableInput {
-        database: Specified(translate_sql_name(db_name_pair)),
-        table: translate_sql_name(table_name_pair),
-        position,
-    }
+    position.holding(Stage2Pine::Select(identifiers::translate_column(
+        column_pair,
+    )))
 }
 
 impl From<Span<'_>> for Position {
---
+++ gen2/rusty-pine/src/syntax/stage2/identifiers.rs
@@ -0,0 +1,131 @@
+use crate::syntax::stage1::Rule;
+use crate::syntax::OptionalInput::{Implicit, Specified};
+use crate::syntax::{ColumnInput, OptionalInput, Position, SqlIdentifierInput, TableInput};
+use pest::iterators::Pair;
+
+pub fn translate_column(column: Pair<Rule>) -> ColumnInput {
+    assert_eq!(Rule::column, column.as_rule());
+
+    let mut inners = column.into_inner();
+    let inner = inners.next().expect("Has to be valid syntax");
+    assert!(inners.next().is_none());
+
+    match inner.as_rule() {
+        Rule::db_table_column_name => translate_db_table_column_name(inner),
+        Rule::table_column_name => translate_table_column_name(inner),
+        Rule::column_name => translate_column_name(inner),
+        _ => panic!("Unknown column type {:#?}", inner.as_rule()),
+    }
+}
+
+fn translate_column_name(pair: Pair<Rule>) -> ColumnInput {
+    assert_eq!(Rule::column_name, pair.as_rule());
+
+    let position: Position = pair.as_span().into();
+
+    let mut inners = pair.into_inner();
+
+    let table = Implicit;
+    let column = translate_sql_name(inners.next().unwrap());
+
+    assert!(inners.next().is_none());
+
+    ColumnInput {
+        table,
+        column,
+        position,
+    }
+}
+
+fn translate_table_column_name(pair: Pair<Rule>) -> ColumnInput {
+    assert_eq!(Rule::table_column_name, pair.as_rule());
+
+    let position: Position = pair.as_span().into();
+
+    let mut inners = pair.into_inner();
+
+    let table_name = translate_sql_name(inners.next().unwrap());
+    let table = Specified(TableInput {
+        table: table_name,
+        position: table_name.position,
+        database: Implicit,
+    });
+    let column = translate_sql_name(inners.next().unwrap());
+
+    ColumnInput {
+        table,
+        column,
+        position,
+    }
+}
+
+fn translate_db_table_column_name(pair: Pair<Rule>) -> ColumnInput {
+    assert_eq!(Rule::db_table_column_name, pair.as_rule());
+
+    let position: Position = pair.as_span().into();
+
+    let mut inners = pair.into_inner();
+
+    let table = Specified(translate_table(inners.next().unwrap()));
+    let column = translate_sql_name(inners.next().unwrap());
+
+    ColumnInput {
+        table,
+        column,
+        position,
+    }
+}
+
+pub fn translate_table(name_pair: Pair<Rule>) -> TableInput {
+    let mut inners = name_pair.into_inner();
+    let inner = inners.next().expect("No pairs should be invalid syntax");
+    assert!(
+        inners.next().is_none(),
+        "Multiple pairs should be invalid syntax"
+    );
+
+    match inner.as_rule() {
+        Rule::sql_name => translate_table_sql_name(inner),
+        Rule::db_table_name => translate_db_table_name(inner),
+        _ => panic!("Unsupported rule: {:?}", inner.as_rule()),
+    }
+}
+
+fn translate_table_sql_name(pair: Pair<Rule>) -> TableInput {
+    assert_eq!(Rule::sql_name, pair.as_rule());
+
+    let position = pair.as_span().into();
+
+    TableInput {
+        database: OptionalInput::Implicit,
+        table: translate_sql_name(pair),
+        position,
+    }
+}
+
+fn translate_sql_name(pair: Pair<Rule>) -> SqlIdentifierInput {
+    assert_eq!(Rule::sql_name, pair.as_rule());
+
+    let position = pair.as_span().into();
+
+    SqlIdentifierInput {
+        name: pair.as_str(),
+        position,
+    }
+}
+
+fn translate_db_table_name(pair: Pair<Rule>) -> TableInput {
+    assert_eq!(Rule::db_table_name, pair.as_rule());
+
+    let position = pair.as_span().into();
+
+    let mut inners = pair.into_inner();
+    let db_name_pair = inners.next().expect("No db should be invalid syntax");
+    let table_name_pair = inners.next().expect("No table should be invalid syntax");
+
+    TableInput {
+        database: Specified(translate_sql_name(db_name_pair)),
+        table: translate_sql_name(table_name_pair),
+        position,
+    }
+}
