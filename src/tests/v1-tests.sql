698141dc90a6598caf41c09f517675ce84f3c66a
Fabian Badoi <fb@penneo.com>
2024-06-15 16:12:37 +0200

    Add old integration tests from the V1 implementation

---
+++ src/tests/v1-tests.sql
@@ -0,0 +1,91 @@
+-- These are the tests I wrote for the V1 version of this tool.
+create table `people` (
+    `id`           int auto_increment,
+    `name`         varchar(256) null,
+    `dateOfBirth`  date         not null,
+    `placeOfBirth` varchar(256) not null,
+    primary key (`id`),
+)
+    charset = utf8mb4;
+
+create table `friendMap` (
+    `friendA`  int                                                                               not null,
+    `friendB`  int                                                                               not null,
+    `strength` enum ('acquaintances', 'friends', 'bffs', 'two kittens sleeping in the same box') not null,
+    primary key (`friendA`, `friendB`),
+    constraint `friendMap_friendA_fk`
+        foreign key (`friendA`) references `people` (`id`),
+    constraint friendMap_friendB_fk
+        foreign key (`friendB`) references `people` (`id`)
+)
+    charset = utf8mb4;
+
+create table `friendshipLog` (
+    `id`       int auto_increment,
+    `friendA`  int  not null,
+    `friendB`  int  not null,
+    `metadata` text null,
+    primary key (`id`),
+    constraint `friendshipLog_friendA_fk`
+        foreign key (`friendA`) references `people` (`id`),
+    constraint friendshipLog_friendB_fk
+        foreign key (`friendB`) references `people` (`id`)
+)
+    charset = utf8mb4;
+
+create table `friendshipProperties` (
+    `id`      int auto_increment,
+    `friendA` int           not null,
+    `friendB` int           not null,
+    `tag`     varchar(64)   not null,
+    `value`   varchar(1024) null,
+    primary key (`id`),
+    constraint friendshipProperties_fk
+        foreign key (`friendA`, `friendB`) references `friendMap` (`friendA`, `friendB`)
+)
+    charset = utf8mb4;
+
+create table `preferences` (
+    `id`       int auto_increment,
+    `personId` int          not null,
+    `value`    varchar(256) not null,
+    primary key (`id`),
+    constraint `preferences_people_fk`
+        foreign key (`personId`) references `people` (`id`)
+)
+    charset = utf8mb4;
+
+create table `preferenceHistory` (
+    `id`           int          not null,
+    `preferenceId` int          not null,
+    `oldValue`     varchar(256) null,
+    primary key (`id`),
+    constraint `preferenceHistory_preferenceId_fk`
+        foreign key (`preferenceId`) references `preferences` (`id`)
+)
+    charset = utf8mb4;
+
+-- Test: people | s: name | preferences
+SELECT people.name, preferences.*
+FROM preferences
+LEFT JOIN people ON people.id = preferences.personId
+LIMIT 10;
+
+-- Test: people | s: id isBlue
+SELECT id, isBlue
+FROM people
+LIMIT 10;
+
+-- Test: people | preferences
+SELECT preferences.*
+FROM preferences
+LEFT JOIN people ON people.id = preferences.personId
+LIMIT 10;
+
+-- Test: people | preferences | friendshipLog
+SELECT friendshipLog.*
+FROM friendshipLog
+LEFT JOIN preferences ON preferences.personId = friendshipLog.friendA
+LEFT JOIN people ON people.id = preferences.personId
+LIMIT 10;
+
